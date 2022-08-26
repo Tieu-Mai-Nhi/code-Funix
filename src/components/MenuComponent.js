7a373977e495b8133bbe0d472947d7d8ec3a2593
Mai Duc Thang <thangmdfx12498@funix.edu.vn>
2022-08-26 11:04:09 +0700

    Lab04.1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,19 +1,13 @@
+// presentation component
+
 import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 import DishDetail from "./DishdetailComponent";
+
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null,
-        };
-    }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });  
-        // dish là tham số của hàm onDishSelected
     }
 
     render() {
@@ -21,7 +15,9 @@ class Menu extends Component {
         const menu  = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card key={dish.id}
+                        onClick={() => this.props.onClick(dish.id)}
+                    >
                         <CardImg width="100%" src={dish.image} alt={dish.name}>
                         </CardImg>
                         <CardImgOverlay>
@@ -36,10 +32,6 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    <DishDetail love={this.state.selectedDish} />
-                    {/* truyền prop love cho DishDetail  */}
                 </div>
             </div>
         );
